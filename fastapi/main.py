e86ac9b63aad3548f2d395ade3989746d6cc7b2e
Saniya <sanya.gusain.19@gmail.com>
2026-08-07 11:21:02 +0530

    Updating delete route

--- fastapi/main.py
+++ fastapi/main.py
@@ -139,16 +139,16 @@ def edit_patient(patient_id: str, patient_update: PatientUpdate):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message':'Patient updated successfully'})
-# @app.delete("/delete/{patient_id}")   
-# def delete_patient(patient_id:str):
-#     data = load_data()
-#     if patient_id not in data:
-#         raise HTTPException(status_code=404, detail='Patient not found')
-#     del data[patient_id]
+@app.delete("/delete/{patient_id}")   
+def delete_patient(patient_id:str):
+    data = load_data()
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    del data[patient_id]
 
-#     save_data(data)
+    save_data(data)
 
-#     return JSONResponse(status_code=200, content={'message':'Patient deleted successfully'})
+    return JSONResponse(status_code=200, content={'message':'Patient deleted successfully'})
 
 
 
